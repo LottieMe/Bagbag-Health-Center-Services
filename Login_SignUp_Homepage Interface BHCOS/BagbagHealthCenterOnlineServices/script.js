623da84de44c99084323cf871db7a9fb952e5a6e
Charlotte Balang <balang.charlotte.abalos@gmail.com>
2024-11-20 23:35:04 +0800

    UI login, signup, homepage
    Coded UI for login, sign up, homepage for the system(no backend & daatbase yet!). Coded by ALEJO

---
+++ Login_SignUp_Homepage Interface BHCOS/BagbagHealthCenterOnlineServices/script.js
@@ -0,0 +1,18 @@
+/* visitingpage */
+function showlogin(){
+        document.querySelector(".bgopacitylogin").style.display = "flex";
+}
+
+function hidelogin(){
+    document.querySelector(".bgopacitylogin").style.display = "none";
+}
+
+function showsignup(){
+    document.querySelector(".bgopacitysignup").style.display = "flex";
+}
+
+function hidesignup(){
+document.querySelector(".bgopacitysignup").style.display = "none";
+}
+
+/* homepage */
